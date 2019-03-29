4fd6ba62705fad77a12b16efb7484e0a3d5ea83b
unknown <luongthanhnhi00@gmail.com>
2019-03-29 21:57:28 +0700

    first commit

---
+++ MvcMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
